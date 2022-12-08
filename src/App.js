9954fdb1557f97f2b62204c7548a9c7f1765df91
vishnu R P <rpvishnu44@gmail.com>
2022-12-08 19:50:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './Components/navBar/NavBar'
+import "./App.css"
+import Banner from './Components/Banner/Banner';
+import Rowpost from './Components/RowPost/Rowpost';
+import {originals,action,romantic,horrer,comedy,treanding,doc} from './urls'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner/> 
+      <Rowpost url={originals} title='Netflix Originals' />
+      <Rowpost url={action} title='Action'  isSmall />
+      <Rowpost url={romantic} title='romantic'  isSmall />
+      <Rowpost url={horrer} title='horrer'  isSmall />
+      <Rowpost url={comedy} title='comedy'  isSmall />
+      <Rowpost url={treanding} title='treanding'  isSmall />
+      <Rowpost url={doc} title='Documentaries'  isSmall />
     </div>
   );
 }
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseurl='https://api.themoviedb.org/3'
+export const API_KEY='59e7864120614057dbef8b323a8ecc06'
+export const imageUrl='https://image.tmdb.org/t/p/original'
